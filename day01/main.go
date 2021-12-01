d4266c77e85e9a6b8bbc0d0756d592ac72e149db
Tobias Böhm <code@aibor.de>
2021-12-01 17:41:56 +0100

    refactor go solution to support part two

--- day01/main.go
+++ day01/main.go
@@ -8,6 +8,12 @@ import (
   "strconv"
 )
 
+var slidwin [4]int
+
+func slidwincomp(dec int, i int) bool {
+  return i > dec && slidwin[i % 4] > slidwin[(i - dec) % 4]
+}
+
 func main() {
   file, err := os.Open("input")
 
@@ -19,17 +25,23 @@ func main() {
 
   scanner := bufio.NewScanner(file)
 
-  var prev, cur, inc int = 0, 0, 0
+  var part_one_inc, part_two_inc, cur, i int = 0, 0, 0, 0
 
   for scanner.Scan() {
     cur, _ = strconv.Atoi(scanner.Text())
+    i++
 
-    if prev != 0 && cur > prev {
-      inc++
+    slidwin[i % 4] = cur
+
+    if slidwincomp(1, i) {
+      part_one_inc++
     }
 
-    prev = cur
+    if slidwincomp(3, i) {
+      part_two_inc++
+    }
   }
 
-  fmt.Println(inc)
+  fmt.Println("Part 1:", part_one_inc)
+  fmt.Println("Part 2:", part_two_inc)
 }
