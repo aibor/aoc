a4067fec74f7574e384927e65c51ee5055666afd
Tobias Böhm <code@aibor.de>
2022-12-04 11:46:26 +0100

    Deduplicate boilerplate test files

--- 2022/newDay.sh
+++ 2022/newDay.sh
@@ -13,7 +13,8 @@ fi
 basedir=$(dirname "${BASH_SOURCE[0]}")
 dir=$(printf "%s/day%02d" "$basedir" "$day")
 
-mkdir "$dir"
+mkdir -v "$dir"
+ln -s ../_puzzle_test.go "$dir/puzzle_test.go"
 
 for f in input example
 do
@@ -55,56 +56,5 @@ func part2(input string) string {
 	}
 
 	return fmt.Sprintf("%d", result)
-}
-EOF
-
-cat > "$dir/puzzle_test.go" <<EOF
-package main
-
-import (
-	_ "embed"
-	"testing"
-)
-
-//go:embed example
-var example string
-
-//go:embed input
-var input string
-
-func assert(t *testing.T, result, expectation string) {
-	t.Helper()
-
-	if result != expectation {
-		t.Fatalf("got %s\n", result)
-	}
-}
-
-func TestPart1Example(t *testing.T) {
-	assert(t, part1(example), exampleResult1)
-}
-
-func TestPart2Example(t *testing.T) {
-	assert(t, part2(example), exampleResult2)
-}
-
-func TestPart1Input(t *testing.T) {
-	assert(t, part1(input), result1)
-}
-
-func TestPart2Input(t *testing.T) {
-	assert(t, part2(input), result2)
-}
-
-func BenchmarkPart1(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		part1(input)
-	}
-}
-
-func BenchmarkPart2(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		part2(input)
-	}
 }
 EOF
