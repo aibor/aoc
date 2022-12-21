bba7d1ea8792c9807a739713d26402c673f074dc
Tobias Böhm <code@aibor.de>
2022-12-21 17:43:07 +0100

    2022 day21 go condense

--- 2022/day21/puzzle.go
+++ 2022/day21/puzzle.go
@@ -18,8 +18,8 @@ var (
 func part1(input string) string {
 	var result int
 
-	ms := parseMonkeys(goutils.SplitInput(input))
-	result = ms["root"].yell()
+	root := parseMonkeys(goutils.SplitInput(input))
+	result = root.yell()
 
 	return strconv.Itoa(result)
 }
@@ -27,27 +27,35 @@ func part1(input string) string {
 func part2(input string) string {
 	var result int
 
-	ms := parseMonkeys(goutils.SplitInput(input))
-	ms["root"].op = "="
-	result = ms["root"].track("humn", 0)
+	root := parseMonkeys(goutils.SplitInput(input))
+	root.op = "="
+	result = root.track("humn", 0)
 
 	return strconv.Itoa(result)
 }
 
-type monkeyOp func(int, int) int
-
-func monkeyOps(op string) monkeyOp {
+func monkeyOp(op string, a, b int) int {
 	switch op {
 	case "+":
-		return func(a, b int) int { return a + b }
+		return a + b
 	case "-":
-		return func(a, b int) int { return a - b }
+		return a - b
 	case "*":
-		return func(a, b int) int { return a * b }
+		return a * b
 	case "/":
-		return func(a, b int) int { return a / b }
+		return a / b
+	case "=":
+		return b
 	}
 	panic("oh no")
+}
+
+var reverseOp = map[string]string{
+	"+": "-",
+	"-": "+",
+	"*": "/",
+	"/": "*",
+	"=": "=",
 }
 
 type monkey struct {
@@ -57,25 +65,9 @@ type monkey struct {
 	dep1, dep2 *monkey
 }
 
-func (m *monkey) backOp(op string) monkeyOp {
-	switch op {
-	case "+":
-		op = "-"
-	case "-":
-		op = "+"
-	case "*":
-		op = "/"
-	case "/":
-		op = "*"
-	case "=":
-		return func(a, b int) int { return b }
-	}
-	return monkeyOps(op)
-}
-
 func (m *monkey) yell() int {
 	if m.num == 0 {
-		m.num = monkeyOps(m.op)(m.dep1.yell(), m.dep2.yell())
+		m.num = monkeyOp(m.op, m.dep1.yell(), m.dep2.yell())
 	}
 	return m.num
 }
@@ -84,15 +76,17 @@ func (m *monkey) track(name string, val int) int {
 	if m.name == name {
 		return val
 	}
-	if m.dep1 != nil && m.dep1.hasDep(name) {
-		return m.dep1.track(name, m.backOp(m.op)(val, m.dep2.yell()))
+
+	var newVal int
+	if m.dep1.hasDep(name) {
+		newVal = monkeyOp(reverseOp[m.op], val, m.dep2.yell())
+		return m.dep1.track(name, newVal)
 	}
-	if m.dep2 != nil && m.dep2.hasDep(name) {
-		var newVal int
+	if m.dep2.hasDep(name) {
 		if m.op == "/" || m.op == "-" {
-			newVal = monkeyOps(m.op)(m.dep1.yell(), val)
+			newVal = monkeyOp(m.op, m.dep1.yell(), val)
 		} else {
-			newVal = m.backOp(m.op)(val, m.dep1.yell())
+			newVal = monkeyOp(reverseOp[m.op], val, m.dep1.yell())
 		}
 		return m.dep2.track(name, newVal)
 	}
@@ -117,8 +111,8 @@ func (ms *monkeys) get(name string) *monkey {
 	return m
 }
 
-func parseMonkeys(lines []string) monkeys {
-	ms := make(monkeys, 8192)
+func parseMonkeys(lines []string) *monkey {
+	ms := make(monkeys, len(lines))
 	for _, l := range lines {
 		f := strings.Fields(l)
 		name := f[0][:4]
@@ -131,5 +125,5 @@ func parseMonkeys(lines []string) monkeys {
 		m.dep1 = ms.get(f[1])
 		m.dep2 = ms.get(f[3])
 	}
-	return ms
+	return ms["root"]
 }
