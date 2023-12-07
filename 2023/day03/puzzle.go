1afefaf6273566b795ae3200665bd133b8da0144
Tobias Böhm <code@aibor.de>
2023-12-07 18:21:48 +0100

    2023 day03 go part 2

--- 2023/day03/puzzle.go
+++ 2023/day03/puzzle.go
@@ -17,7 +17,8 @@ func part1(input string) string {
 	for lineNum, line := range em {
 		matches := re.FindAllStringIndex(line, -1)
 		for _, match := range matches {
-			if em.isPartNumber(lineNum, match[0], match[1]) {
+			num := number{point{lineNum, match[0]}, match[1]}
+			if em.check(num, em.isSymbol) != nil {
 				result += goutils.MustBeInt(line[match[0]:match[1]])
 			}
 		}
@@ -28,9 +29,25 @@ func part1(input string) string {
 
 func part2(input string) string {
 	var result int
+	gears := make(map[point][]int)
 
-	for _, line := range goutils.SplitInput(input) {
-		_ = line
+	re := regexp.MustCompile(`\d+`)
+
+	em := engineMap(goutils.SplitInput(input))
+	for lineNum, line := range em {
+		matches := re.FindAllStringIndex(line, -1)
+		for _, match := range matches {
+			num := number{point{lineNum, match[0]}, match[1]}
+			if p := em.check(num, em.isGearSymbol); p != nil {
+				gears[*p] = append(gears[*p], goutils.MustBeInt(line[match[0]:match[1]]))
+			}
+		}
+	}
+
+	for _, nums := range gears {
+		if len(nums) == 2 {
+			result += nums[0] * nums[1]
+		}
 	}
 
 	return strconv.Itoa(result)
@@ -38,30 +55,58 @@ func part2(input string) string {
 
 type engineMap []string
 
-func (em *engineMap) isPartNumber(lineNum, colStart, colNext int) bool {
-	if em.isSymbol(lineNum, colStart-1) || em.isSymbol(lineNum, colNext) {
-		return true
+type point struct {
+	line int
+	col  int
+}
+
+type number struct {
+	point
+	colNext int
+}
+
+func (em *engineMap) check(num number, f func(int, int) bool) *point {
+	if num.col > 0 && f(num.line, num.col-1) {
+		return &point{num.line, num.col - 1}
 	}
-	for _, l := range []int{lineNum - 1, lineNum + 1} {
-		for c := colStart - 1; c <= colNext; c++ {
-			if em.isSymbol(l, c) {
-				return true
+	if num.colNext < len((*em)[0]) && f(num.line, num.colNext) {
+		return &point{num.line, num.colNext}
+	}
+
+	scanLine := func(l int) *point {
+		for c := num.col - 1; c <= num.colNext; c++ {
+			if c < 0 || c >= len((*em)[0]) {
+				continue
+			}
+			if f(l, c) {
+				return &point{l, c}
 			}
 		}
+		return nil
 	}
-	return false
+
+	if num.line > 0 {
+		if p := scanLine(num.line - 1); p != nil {
+			return p
+		}
+	}
+	if num.line < len(*em)-1 {
+		if p := scanLine(num.line + 1); p != nil {
+			return p
+		}
+	}
+	return nil
 }
 
 func (em *engineMap) isSymbol(lineNum, colNum int) bool {
-	if lineNum < 0 || lineNum >= len(*em) {
-		return false
-	}
-	if colNum < 0 || colNum >= len((*em)[0]) {
-		return false
-	}
 	r := (*em)[lineNum][colNum]
 	if r == '.' {
 		return false
 	}
 	return !unicode.IsDigit(rune(r))
+}
+
+func (em *engineMap) isGearSymbol(lineNum, colNum int) bool {
+	r := (*em)[lineNum][colNum]
+	return r == '*'
 }
--- 2023/day03/results.go
+++ 2023/day03/results.go
@@ -2,7 +2,7 @@ package main
 
 var (
 	exampleResult1 = "4361"
-	exampleResult2 = "0"
+	exampleResult2 = "467835"
 
 	result1 = "559667"
 	result2 = "0"
