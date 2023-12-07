25c8a808b756db06359794b2aee8d8809eb4d576
Tobias Böhm <code@aibor.de>
2023-12-07 15:46:14 +0100

    2023 day05 go part 1

---
+++ 2023/day05/puzzle.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/aibor/aoc/goutils"
+)
+
+func part1(input string) string {
+	var result int
+
+	lines := goutils.SplitInput(input)
+	alm := parseAlmanac(lines[2:])
+	for _, seed := range strings.Split(lines[0], " ")[1:] {
+		value := alm.run(goutils.MustBeInt(seed))
+		if result == 0 || value < result {
+			result = value
+		}
+	}
+
+	return strconv.Itoa(result)
+}
+
+func part2(input string) string {
+	var result int
+
+	for _, line := range goutils.SplitInput(input) {
+		_ = line
+	}
+
+	return strconv.Itoa(result)
+}
+
+type mapRange struct {
+	length           int
+	sourceStart      int
+	destinationStart int
+}
+
+type valueMap struct {
+	name   string
+	ranges []mapRange
+}
+
+func (m *valueMap) run(value int) int {
+	for _, r := range m.ranges {
+		if value < r.sourceStart {
+			continue
+		}
+		if value >= r.sourceStart+r.length {
+			continue
+		}
+		value = r.destinationStart + (value - r.sourceStart)
+		break
+	}
+	return value
+}
+
+type almanac []*valueMap
+
+func (a *almanac) run(value int) int {
+	for _, m := range *a {
+		value = m.run(value)
+	}
+	return value
+}
+
+func parseAlmanac(input []string) almanac {
+	var curMap *valueMap
+	var alm almanac
+
+	for _, line := range input {
+		switch {
+		case line == "":
+		case strings.HasSuffix(line, ":"):
+			curMap = &valueMap{
+				name: line[:len(line)-1],
+			}
+			alm = append(alm, curMap)
+		default:
+			fields := strings.Split(line, " ")
+			curMap.ranges = append(curMap.ranges, mapRange{
+				goutils.MustBeInt(fields[2]),
+				goutils.MustBeInt(fields[1]),
+				goutils.MustBeInt(fields[0]),
+			})
+		}
+	}
+
+	return alm
+}
---
+++ 2023/day05/results.go
@@ -0,0 +1,9 @@
+package main
+
+var (
+	exampleResult1 = "35"
+	exampleResult2 = "0"
+
+	result1 = "165788812"
+	result2 = "0"
+)
